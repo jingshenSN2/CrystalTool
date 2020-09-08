4465fc5cf9ae8891230191185d0645741d4c568d
jingshenSN2 <512320254@qq.com>
2020-09-08 13:48:18 +0800

    graph visualization with fixed position and label.

--- GraphConverter.py
+++ GraphConverter.py
@@ -4,7 +4,7 @@ import networkx as nx
 def graph_converter(cell):
     g = nx.Graph()
     for atom in cell.atom_list:
-        g.add_node(atom)
+        g.add_node(atom, location=(atom.x, atom.y), label=atom.element)
         for neighbor in atom.neighbors.keys():
             if (atom, neighbor) not in g.edges:
                 g.add_edge(atom, neighbor, dist=atom.neighbors[neighbor])
--- test.py
+++ test.py
@@ -4,14 +4,16 @@ import networkx as nx
 import CrystalParser
 import GraphConverter
 
-cell = CrystalParser.parse_res('c21.res')
+cell = CrystalParser.parse_res('c21_origin.res')
 cell.calc_neighbors()
 graph = GraphConverter.graph_converter(cell)
 c = max(nx.connected_components(graph), key=len)
 graph = graph.subgraph(c)
 plt.figure(figsize=(10, 9))
-pos = nx.random_layout(graph)
+pos = nx.get_node_attributes(graph, 'location')
+label = nx.get_node_attributes(graph, 'label')
 nx.draw_networkx(graph, pos, alpha=0.7, with_labels=False, edge_color='.4')
+nx.draw_networkx_labels(graph, pos, labels=label)
 plt.axis('off')
 plt.tight_layout()
 plt.show()
