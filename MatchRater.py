e57e0c21af012c182222c0b7f9a003806dcd24d8
Jingshen <js_gao@pku.edu.cn>
2020-10-07 20:37:51 +0800

    working on match&rate system.

--- MatchRater.py
+++ MatchRater.py
@@ -1,32 +1,50 @@
 from networkx.algorithms.isomorphism import GraphMatcher
+import networkx as nx
+
+
+def node_match(atom1,atom2):
+    cno = ['C','N','O','Q']
+    if atom1['element'] in cno and atom2['element'] in cno:
+        return True
+    return atom1['element'] == atom2['element']
+
+
+def edge_match(edge1, edge2):
+    return True
+
+
+def shrink_one(graph):
+    newgraph = nx.Graph(graph)
+    degree = newgraph.degree
+    flag = False
+    for node in newgraph.nodes():
+        if degree[node] == 1:
+            newgraph.remove_node(node)
+            flag = True
+            break
+    return flag, newgraph
 
 
 class MatchRater:
-    def __init__(self, target, query, node_match=None, edge_match=None):
+    def __init__(self, target, query, node_match=node_match, edge_match=edge_match):
         self.target = target
         self.query = query
         self.result = None
         self.node_match = node_match
         self.edge_match = edge_match
 
-    def _is_subgraph(self, g1, g2):
-        gm = GraphMatcher(g1, g2, node_match=self.node_match,
-                          edge_match=self.edge_match)
-        return gm.subgraph_is_isomorphic()
-
     def match(self):
-        if self._is_subgraph(self.target, self.query):
-            self.result = self.query
+        gm = GraphMatcher(self.target, self.query, node_match=self.node_match,
+                          edge_match=self.edge_match)
+        while not gm.subgraph_is_isomorphic():
+            flag, self.query = shrink_one(self.query)
+            if not flag:
+                print('匹配失败')
+                return False
+            gm = GraphMatcher(self.target, self.query, node_match=self.node_match,
+                          edge_match=self.edge_match)
+        return gm.subgraph_isomorphisms_iter()
 
-    def _shrink_match(self, sub_query):
-        degree = sub_query.degree
-
-    def _shrink_one(self, graph):
-        degree = graph.degree
-        d1_nodes = set()
-        for node in graph.nodes():
-            if degree[node] == 1:
-                d1_nodes.add(node)
 
 
 
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import CrystalParser
 import GraphHandler
+import MatchRater
 
 
 cell = CrystalParser.parse_res('test/c21_origin.res')
@@ -7,8 +8,8 @@ cell.calc_neighbors()
 graph = GraphHandler.graph_converter(cell)
 target = GraphHandler.max_subgraph(graph)
 GraphHandler.draw_graph(target, direction='a')
-GraphHandler.draw_graph(target, direction='b')
-GraphHandler.draw_graph(target, direction='c')
+#GraphHandler.draw_graph(target, direction='b')
+#GraphHandler.draw_graph(target, direction='c')
 
 
 pattern = CrystalParser.parse_pdb('test/C15H21NO3S_C2.pdb')
@@ -16,6 +17,15 @@ pattern.calc_neighbors()
 graph2 = GraphHandler.graph_converter(pattern)
 query = GraphHandler.max_subgraph(graph2)
 GraphHandler.draw_graph(query, direction='a')
-GraphHandler.draw_graph(query, direction='b')
-GraphHandler.draw_graph(query, direction='c')
+#GraphHandler.draw_graph(query, direction='b')
+#GraphHandler.draw_graph(query, direction='c')
 
+
+mr = MatchRater.MatchRater(target,query)
+result = mr.match()
+
+for i in result:
+    print(i)
+    GraphHandler.draw_graph_highlight(target,i)
+
+print(1)
