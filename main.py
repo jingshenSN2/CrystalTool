42d8130ef95cf176b2996a7fbfa041ad73eaa8ce
Jingshen <js_gao@pku.edu.cn>
2020-10-15 21:23:57 +0800

    Modified .gitignore

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 
 
 def run_task(id, setting):
-    silent = (setting['silent'] == '1')
-    print('开始执行task:%s' % id) if not silent else ''
+    silent = (setting['silent'] == 'True')
+    print('开始执行%s' % id) if not silent else ''
     print('读取res文件%s...' % setting['target']) if not silent else ''
     target = GH.graph_converter(CP.parse_res(setting['target']))
     print('读取pdb文件%s...' % setting['query']) if not silent else ''
@@ -32,14 +32,14 @@ def run_task(id, setting):
     if result:
         rmsd, best_result = MR.best_result(result)
         GH.draw_graph_highlight(target, best_result)
-        plt.title('task:%s target=%s query=%s match_mode=%s loss_atom=%s rmsd=%.2f' % (id, setting['target'], setting['query'], match, setting['loss'], rmsd))
+        plt.title('%s target=%s query=%s match_mode=%s loss_atom=%s rmsd=%.2f' % (id, setting['target'], setting['query'], match, setting['loss'], rmsd))
         if setting['fig'] in ('1', '2'):
             os.makedirs(setting['output_path'], exist_ok=True)
-            plt.savefig(setting['output_path'] + id + '.jpg')
+            plt.savefig(setting['output_path'] + id.strip('task:') + '.jpg')
         if setting['fig'] == '2':
             plt.show()
         plt.close()
-    print('True %.2f' % rmsd) if result else print('False')
+    print('%s True %.2f' % (id, rmsd)) if result else print('%s False' % id)
 
 
 def main(argv):
@@ -54,11 +54,10 @@ def main(argv):
     section.remove('global')
     global_setting = dict(cfp.items('global'))
     for task in section:
-        id = task.strip('task:')
         setting = global_setting.copy()
         for k, v in cfp.items(task):
             setting[k] = v
-        run_task(id, setting)
+        run_task(task, setting)
 
 
 if __name__ == '__main__':
