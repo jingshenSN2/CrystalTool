2eaca2933a336fceb87caf020479f2b0392aa4ce
gaojingsong <gaojingsong@bytedance.com>
2021-05-21 21:19:16 +0800

    feat: add icon

--- crystaltoolgui/libs/__init__.py
+++ crystaltoolgui/libs/__init__.py
@@ -1,10 +1,36 @@
 from .singleton import singleton
 
-from PyQt5.QtCore import QRect, pyqtSignal, Qt, QThread, QStringListModel
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QRect
+from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QStringListModel
+
+from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QStandardItemModel
 
-from PyQt5.QtWidgets import QFormLayout, QGridLayout, QHBoxLayout
-from PyQt5.QtWidgets import QWidget, QLabel, QMainWindow
-from PyQt5.QtWidgets import QPushButton, QRadioButton, QSpinBox
+# layout
+from PyQt5.QtWidgets import QFormLayout
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout
+
+# window
 from PyQt5.QtWidgets import QFileDialog
-from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QAbstractItemView, QHeaderView, QTabWidget
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QTabWidget
+
+# widget
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
+
+# button
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QRadioButton
+from PyQt5.QtWidgets import QSpinBox
+
+# table
+from PyQt5.QtWidgets import QHeaderView
+from PyQt5.QtWidgets import QTableWidget
+from PyQt5.QtWidgets import QTableWidgetItem
+from PyQt5.QtWidgets import QAbstractItemView
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
 from crystaltoolgui import MainUI
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon('crystaltoolgui/icon.png'))
     window = MainUI()
     window.show()
     sys.exit(app.exec_())
